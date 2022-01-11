637dc89636189be5518fce6c2a868b656fcc0606
Itward10 <89509863+Itward10@users.noreply.github.com>
2022-01-11 14:20:25 +0100

    Add files via upload

---
+++ M226b/AnimalShelter/Hund.py
@@ -0,0 +1,7 @@
+from Tier import Tier
+
+class Hund(Tier):
+
+    def __init__(self, name, farbe, funddatum):
+
+        super().__init__(name, farbe, funddatum)
---
+++ M226b/AnimalShelter/Katze.py
@@ -0,0 +1,9 @@
+from Tier import Tier
+
+class Katze(Tier):
+
+    def __init__(self, name, farbe, funddatum):
+
+        super().__init__(name, farbe, funddatum)
+
+
