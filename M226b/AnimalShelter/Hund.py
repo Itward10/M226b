8d32b37b3c45876e0dbc1ffd26d8c81a41a59f6d
Itward10 <89509863+Itward10@users.noreply.github.com>
2022-01-21 20:28:39 +0100

    Delete M226b/AnimalShelter directory

--- M226b/AnimalShelter/Hund.py
+++
@@ -1,7 +0,0 @@
-from Tier import Tier
-
-class Hund(Tier):
-
-    def __init__(self, name, farbe, funddatum):
-
-        super().__init__(name, farbe, funddatum)
--- M226b/AnimalShelter/Katze.py
+++
@@ -1,9 +0,0 @@
-from Tier import Tier
-
-class Katze(Tier):
-
-    def __init__(self, name, farbe, funddatum):
-
-        super().__init__(name, farbe, funddatum)
-
-
